c3df50b558d9f59ca8a6c93395eb10aee8f630b4
JarrodKane <kajarrod@gmail.com>
2021-06-03 14:23:34 +1000

    - Added card details

---
+++ components/molecules/CardEDetail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  episode: [];
+}
+
+const CardEDetail: React.FC<Props> = ({ episode }) => {
+  return <div></div>;
+};
+
+export default CardEDetail;
