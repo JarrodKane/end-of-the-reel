1d7627dc57e0a335217e890aa9c64484aa33154a
JarrodKane <kajarrod@gmail.com>
2021-06-03 11:44:14 +1000

    - Added in the static props for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,38 @@ import Image from "next/image";
 // import styles from '../styles/Home.module.css'
 import Base from "../components/templates/Base";
 
-export default function Home() {
+import { getEpisodes } from "./api/podcast";
+
+interface Props {
+  episodes: [];
+  episodeList: [];
+}
+
+export const getStaticProps = async () => {
+  const res = await getEpisodes();
+
+  return {
+    props: {
+      episodes: res.items,
+    },
+    revalidate: 1,
+  };
+};
+
+const Home: React.FC<Props> = ({ episodes }) => {
+  //TODO: Allow for a choice of how many episodes they want shown on the homepage
+  const newList = episodes.slice(0, 3);
   return (
     <Base>
-      {/* <div className={`w-full md:w-auto`}>Image Header</div> */}
       <div>Search Bar here</div>
+      {newList.map((episode) => (
+        <div>{episode.title}</div>
+      ))}
       <div>Episodes, one per row with breaks inbetween, max 3 on page</div>
       <div>Call to action thingy</div>
       <div>Footer information</div>
     </Base>
   );
-}
+};
+
+export default Home;
