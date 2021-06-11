3ec2dee8a07f45ab8e0cd589bcaf1b01f7baef2c
jarrodkane <kajarrod@gmail.com>
2021-06-11 22:43:54 +1000

    - commented out code not in use

--- components/organisims/Nav.tsx
+++ components/organisims/Nav.tsx
@@ -30,7 +30,7 @@ interface Props {}
 const Nav = (props: Props) => {
   const { isOpen, handleModal } = useContext(ModalContext);
 
-  const window = useWindowSize();
+  // const window = useWindowSize();
 
   return (
     <div
@@ -51,45 +51,45 @@ const Nav = (props: Props) => {
     </div>
   );
 
-  if (window.width !== undefined && window.width < 1024) {
-    return (
-      <div
-        className={`sticky top-0 z-20 flex justify-center text-align bg-white py-1 px-5 md:px-2 shadow-md opacity-90`}
-      >
-        <div className={`fixed top-14 z-30 bg-blue-600 h-screen w-screen`}>
-          dsfdsf
-        </div>
-        <div
-          className={`flex h-12 w-12 cursor-pointer`}
-          onClick={() => handleModal()}
-        >
-          <Hamburger />
-        </div>
-        <div className={`flex w-full justify-center `}>
-          <NavTitle title="End of the Reel" url="/" />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        className={`grid grid-flow-col grid-cols-4 text-align bg-white py-1 px-5`}
-      >
-        <div className={`flex`}>
-          <NavTitle title="End of the Reel" url="/" />
-        </div>
+  // if (window.width !== undefined && window.width < 1024) {
+  //   return (
+  //     <div
+  //       className={`sticky top-0 z-20 flex justify-center text-align bg-white py-1 px-5 md:px-2 shadow-md opacity-90`}
+  //     >
+  //       <div className={`fixed top-14 z-30 bg-blue-600 h-screen w-screen`}>
+  //         dsfdsf
+  //       </div>
+  //       <div
+  //         className={`flex h-12 w-12 cursor-pointer`}
+  //         onClick={() => handleModal()}
+  //       >
+  //         <Hamburger />
+  //       </div>
+  //       <div className={`flex w-full justify-center `}>
+  //         <NavTitle title="End of the Reel" url="/" />
+  //       </div>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div
+  //       className={`grid grid-flow-col grid-cols-4 text-align bg-white py-1 px-5`}
+  //     >
+  //       <div className={`flex`}>
+  //         <NavTitle title="End of the Reel" url="/" />
+  //       </div>
 
-        <div className={`col-span-2 flex items-center place-content-center`}>
-          <StyledLink url="/episodes" text="Episodes" />
-          <LinkSlash />
-          <StyledLink url="/about" text="About" />
-          <LinkSlash />
-          <StyledLink url="/contact" text="Contact" />
-        </div>
-        <SocialIcons width={35} height={35} />
-      </div>
-    );
-  }
+  //       <div className={`col-span-2 flex items-center place-content-center`}>
+  //         <StyledLink url="/episodes" text="Episodes" />
+  //         <LinkSlash />
+  //         <StyledLink url="/about" text="About" />
+  //         <LinkSlash />
+  //         <StyledLink url="/contact" text="Contact" />
+  //       </div>
+  //       <SocialIcons width={35} height={35} />
+  //     </div>
+  //   );
+  // }
 };
 
 export default Nav;
