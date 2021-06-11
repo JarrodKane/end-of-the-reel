653615352a2d754f0049b3f239c8427ec46a55ab
JarrodKane <kajarrod@gmail.com>
2021-06-11 16:56:30 +1000

    Changes to modal

--- components/templates/ContentWrap.tsx
+++ components/templates/ContentWrap.tsx
@@ -5,11 +5,31 @@ import { useRouter } from "next/router";
 import Nav from "../organisims/Nav";
 import Hero from "../molecules/Hero";
 import Footer from "../organisims/Footer";
-import Modal from "../molecules/Modal";
+// import Modal from "../molecules/Modal";
+import ModalLink from "../atoms/ModalLink";
 
 import { ModalContext } from "../../context/ModalProvider";
 
 interface Props {}
+
+const Modal = () => {
+  return (
+    // <div
+    //   className={`fixed top-14 z-50  h-screen  w-screen bg-red-500 `}
+    //   // style={divStyle}
+    // >
+    <div
+      className={`fixed top-14 z-50 flex flex-col  justify-center text-5xl bg-red-500 w-full h-full `}
+    >
+      <ModalLink url="/episodes" text="Episodes" />
+
+      <ModalLink url="/about" text="About" />
+
+      <ModalLink url="/contact" text="Contact" />
+    </div>
+    // </div>
+  );
+};
 
 const ContentWrap: React.FC<Props> = ({ children }) => {
   const { isOpen } = useContext(ModalContext);
