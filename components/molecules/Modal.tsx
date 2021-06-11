f2592b35ec143b0bb5ffb3c6ea065e0d3e7c8d90
JarrodKane <kajarrod@gmail.com>
2021-06-11 15:11:07 +1000

    - Modal change to fix

--- components/molecules/Modal.tsx
+++ components/molecules/Modal.tsx
@@ -3,18 +3,12 @@ import ModalLink from "../atoms/ModalLink";
 
 interface Props {}
 
-const Modal = (props: Props) => {
+const Modal = () => {
   return (
-    <div
-      className={`absolute flex justify-center items-center top-14 z-30 inset-0 overflow-y-auto  w-screen bg-blue-200 `}
-    >
-      <div
-        className={`flex flex-col items-center justify-around text-5xl w-full`}
-      >
+    <div className={`fixed top-14 z-30  h-screen  w-screen bg-blue-200 `}>
+      <div className={`flex flex-col  justify-center text-5xl w-full h-full`}>
         <ModalLink url="/episodes" text="Episodes" />
-
         <ModalLink url="/about" text="About" />
-
         <ModalLink url="/contact" text="Contact" />
       </div>
     </div>
