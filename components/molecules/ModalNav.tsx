91ac0a593c0ea8c1060032936553d1d73bba6c67
JarrodKane <kajarrod@gmail.com>
2021-06-11 21:55:07 +1000

    - removed all inline styles

--- components/molecules/ModalNav.tsx
+++ components/molecules/ModalNav.tsx
@@ -4,18 +4,12 @@ import { ModalContext } from "../../context/ModalProvider";
 
 interface Props {}
 
-const divStyle = {
-  backgroundColor: "#ef4444",
-  zindex: 50,
-};
-
 const ModalNav = () => {
   const { isOpen } = useContext(ModalContext);
 
   return (
     <div
-      className={`fixed top-14 z-30  flex flex-col  justify-center text-5xl  w-screen h-screen  `}
-      style={divStyle}
+      className={`fixed top-14 z-30  flex flex-col bg-red-500  justify-center text-5xl  w-screen h-screen  `}
     >
       <ModalLink url="/episodes" text="Episodes" />
       <ModalLink url="/about" text="About" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -17,6 +18,19 @@ module.exports = {
         Nav: ["Roboto", "sans-serif"],
       },
     },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
   },
   variants: {
     extend: {},
