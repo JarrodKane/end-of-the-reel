60e4d47676432d6a9e6e91d476fa289858fae45b
JarrodKane <kajarrod@gmail.com>
2021-06-11 17:17:36 +1000

    - changing postcss

--- components/templates/ContentWrap.tsx
+++ components/templates/ContentWrap.tsx
@@ -5,23 +5,16 @@ import { useRouter } from "next/router";
 import Nav from "../organisims/Nav";
 import Hero from "../molecules/Hero";
 import Footer from "../organisims/Footer";
-// import Modal from "../molecules/Modal";
-// import ModalLink from "../atoms/ModalLink";
-
-// import { ModalContext } from "../../context/ModalProvider";
 
 interface Props {}
 
 const ContentWrap: React.FC<Props> = ({ children }) => {
-  // const { isOpen } = useContext(ModalContext);
-
   const router = useRouter();
 
   return (
     <div className={`w-full bg-gray-100 `}>
       <Nav />
       <Hero path={router.pathname} />
-      {/* {isOpen ? <Modal /> : null} */}
 
       {children}
 
