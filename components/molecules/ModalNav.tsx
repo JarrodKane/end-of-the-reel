b973a2e64dbf9ff0a8f8c456f89a420433eaab49
jarrodkane <kajarrod@gmail.com>
2021-06-12 19:28:05 +1000

    - Added in a home to the mobile menu

--- components/molecules/ModalNav.tsx
+++ components/molecules/ModalNav.tsx
@@ -1,18 +1,19 @@
 import React, { useContext } from "react";
 import ModalLink from "../atoms/ModalLink";
-import { ModalContext } from "../../context/ModalProvider";
+// import { ModalContext } from "../../context/ModalProvider";
 
 interface Props {}
 
 const ModalNav = () => {
-  const { isOpen } = useContext(ModalContext);
+  // const { isOpen } = useContext(ModalContext);
 
   return (
-    <div className={`relative `}>
+    <div className={`relative`}>
       <div
         className={`fixed top-14 z-30 bg-red-500 h-screen w-screen text-3xl `}
       >
-        <div className={`w-full h-full flex flex-col justify-center`}>
+        <div className={`w-full h-full flex flex-col justify-center -mt-10`}>
+          <ModalLink url="/" text="Home" />
           <ModalLink url="/episodes" text="Episodes" />
           <ModalLink url="/about" text="About" />
           <ModalLink url="/contact" text="Contact" />
